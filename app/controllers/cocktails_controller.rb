a71bb90d1d45adef3adf68069cda7dd524004cf8
Maria Robles <m.robles016@gmail.com>
2018-11-02 16:02:18 +0100

    reseeded the thing and also got images form to work

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -14,7 +14,7 @@ class CocktailsController < ApplicationController
   def create
     @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
-      redirect_to new_cocktail_dose_path(@cocktail) #redirect to add dose
+      redirect_to new_cocktail_dose_path(@cocktail)
     else
       render :new
     end
